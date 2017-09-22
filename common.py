62e4d766f9c40f296bae17c3e84d28e792d65d50
Radomir Matveev <radoma@mail2tor.com>
2017-09-22 15:23:46 +0200

    Begin implementing the interface to the game logic layer

--- common.py
+++ common.py
@@ -9,6 +9,7 @@ Copyright (C) 2017 Radomir Matveev GPL 3.0+
 # --------------------------------------------------------------------------- #
 from PyQt5.QtWidgets import (QApplication, QWidget, QLabel, QProgressBar,
                              QFormLayout, QGridLayout)
+from PyQt5.QtCore import QObject
 #from PyQt5.QtGui import QPixmap
 
 
@@ -16,6 +17,8 @@ from PyQt5.QtWidgets import (QApplication, QWidget, QLabel, QProgressBar,
 # Define classes
 # --------------------------------------------------------------------------- #
 class QProgressList(QWidget):
+    """A list of progress bars with icons and labels.
+    """
     def __init__(self, translation_context, parent=None):
         super().__init__(parent)
         assert isinstance(translation_context, str), "not a string"
@@ -45,6 +48,24 @@ class QProgressList(QWidget):
             label.setText(tra(self.translation_context, text))
 
 
+class WorldInterface(QObject):
+    """Serves data from the game logic layer to the python layer.
+
+    # TODO: actually connect to C++ and fetch the data from there
+    """
+    def __init__(self):
+        super().__init__()
+
+    def schoolDays(self):
+        """Returns a tuple of days were the school is open."""
+        return ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+
+    def timeTablePeriods(self):
+        """Return a tuple of periods for the timetable of the school."""
+        return ("7:50 - 9:20", "9:40 - 11:10", "11:30 - 13:00",
+                "14:30 - 16:00")
+
+
 # --------------------------------------------------------------------------- #
 # Define functions
 # --------------------------------------------------------------------------- #
@@ -55,3 +76,9 @@ def translate_form(form, context, labels):
     for row, text in enumerate(labels):
         label = form.itemAt(row, QFormLayout.LabelRole).widget()
         label.setText(tra(context, text))
+
+
+# --------------------------------------------------------------------------- #
+# Declare module globals
+# --------------------------------------------------------------------------- #
+world = WorldInterface()
